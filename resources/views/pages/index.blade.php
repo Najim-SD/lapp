84ea35d0285c686348696a4cbf7759ec7f4a236c
Epsilon <dev.djvan@gmail.com>
2020-04-18 07:05:26 +0200

    pages and controller routing

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="{{ config('app.locale') }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>{{config('app.name', 'LAPP')}}</title>
+    </head>
+
+    <body>
+        <h1> Hello </h1>
+        <p> there is nothing on this website yet! </p>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,12 @@
 |
 */
 
-Route::get('/', function () {
+Route::get('/', "PagesController@index"); 
+
+Route::get('/about', "PagesController@about");
+
+Route::get('/services', "PagesController@services");
+
+Route::get('/welcome', function () {
     return view('welcome');
 });
