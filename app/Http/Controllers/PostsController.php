88bd0709b1e44cd40b6a27d1f2dac47392d7411f
Epsilon <dev.djvan@gmail.com>
2020-04-18 19:38:06 +0200

    order posts by id  And  add Pagination

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -14,7 +14,7 @@ class PostsController extends Controller
      */
     public function index()
     {
-        $posts = Post::all();
+        $posts = Post::orderBy('id', 'desc')->paginate(5);
         return view("posts.index")->with('posts', $posts);
     }
 
