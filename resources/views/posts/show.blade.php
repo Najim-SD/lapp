2d3531345aa3fe8791320c60da7f371e337e4af3
Epsilon <dev.djvan@gmail.com>
2020-04-19 18:47:09 +0200

    edit and delete posts

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,7 +2,14 @@
 
 @section('content')
     <div class="jumbotron">
-        <a class="btn btn-primary btn-lg" href="/posts" role="button"><-Back</a>
+        <a class="btn btn-primary" href="/posts" role="button"><-Back</a> 
+        <a href="/posts/{{$post->id}}/edit" class="btn btn-primary"> Edit </a>
+
+        {!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'pull-right'])!!}
+            {{Form::hidden('_method', 'DELETE')}}
+            {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
+        {!!Form::close()!!}
+
         <h2 style="text-align:center"> <u> {{$post->title}} </u> </h2>
         <p> {!!$post->body!!} </p>
         <hr>
